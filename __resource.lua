b2ef60e830d005414aa10d45915ab71ac5ac7ad9
Csucsi <csucsi.eth@protonmail.com>
2023-05-22 17:07:48 +0200

    init

--- __resource.lua
+++ __resource.lua
@@ -11,11 +11,11 @@ files {
     "data/**/*"
 }
 
-data_file 'VEHICLE_LAYOUTS_FILE' 'vehicles/data/**/vehiclelayouts.meta'
-data_file 'HANDLING_FILE' 'vehicles/data/**/handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'vehicles/data/**/vehicles.meta'
-data_file 'CARCOLS_FILE' 'vehicles/data/**/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'vehicles/data/**/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
 -- data_file 'DLCTEXT_FILE' 'vehicles/data/**/dlctext.meta'
 -- data_file 'CARCONTENTUNLOCKS_FILE' 'vehicles/data/**/carcontentunlocks.meta'
 -- data_file 'JDM_RIMS_CONTENT_UNLOCKS' 'vehicles/data/**/jdmrimscontentunlocks.meta'
